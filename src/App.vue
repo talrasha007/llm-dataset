6f46a2da53f93c46e39eba644f8b4147b70d5478
Tal <talrasha007@gmail.com>
2025-04-22 15:55:44 +0800

    init .

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ConfigProvider } from 'ant-design-vue'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <ConfigProvider>
+    <div>foo</div>
+  </ConfigProvider>
+</template>
+
+<style scoped>
+</style>
