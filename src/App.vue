fbfe83d256ae44145a3713cee6ff399d24fe57b3
Tal <talrasha007@gmail.com>
2025-04-22 18:21:12 +0800

    layout.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <script setup lang="ts">
-import { ConfigProvider } from 'ant-design-vue'
+import { ref, h } from 'vue'
+import type { MenuProps } from 'ant-design-vue';
+import { Layout, Row, Col, LayoutHeader, LayoutContent, Menu, ConfigProvider } from 'ant-design-vue'
+import { ApartmentOutlined, DatabaseOutlined } from '@ant-design/icons-vue'
+
+const items = ref<MenuProps['items']>([
+  {
+    key: 'sync',
+    icon: () => h(DatabaseOutlined),
+    label: h('a', { href: '#/sync' }, 'SYNC')
+  },
+])
 </script>
 
 <template>
   <ConfigProvider>
-    <div>foo</div>
+    <Layout>
+      <LayoutHeader>
+        <Row class="top-nav">
+          <Col flex="none"><img src="/favicon.svg" class="logo" /></Col>
+          <Col flex="none">
+            <Menu
+              :items="items"
+              mode="horizontal"
+              theme="dark"
+            />
+          </Col>
+        </Row>
+      </LayoutHeader>
+      <LayoutContent>
+        bbb
+      </LayoutContent>
+    </Layout>
   </ConfigProvider>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.top-nav {
+  color: white;
+
+  .logo {
+    height: 30px;
+    margin-top: 17px;
+  }
+}
 </style>
