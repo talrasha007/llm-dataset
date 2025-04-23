fcf2101dee0034c459cd1d9821f08280971ad966
Tal <talrasha007@gmail.com>
2025-04-23 12:21:57 +0800

    display datasets.

--- src/Home.vue
+++ src/Home.vue
@@ -1,20 +1,30 @@
 
 <template>
-  <Row :gutter="16">
+  <Row :gutter="[16, 24]">
+    <Col span="6" v-for="ds of datasets">
+      <Card :title="ds.name">
+        <p>{{ ds.description }}</p>
+      </Card>
+    </Col>
     <Col span="6">
-      <Card title="Add New Dataset" @click="showDatasetModal=true" style="cursor: pointer;"></Card>
+      <Card title="Add New Dataset" @click="showDatasetModal=true" style="cursor: pointer;">
+        <p style="text-align: center;">+</p>
+      </Card>
     </Col>
   </Row>
   <DatasetModal title="Add Dataset" v-model:show="showDatasetModal" />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-// import { liveQuery } from "dexie"
+import { ref, type Ref } from 'vue'
+import { liveQuery } from "dexie"
 import { Row, Col, Card } from 'ant-design-vue'
-// import db from './db'
-// import { useObservable } from "@vueuse/rxjs"
+import db, { type Dataset } from './db'
+import { from } from 'rxjs'
+import { useObservable } from '@vueuse/rxjs'
+
 import DatasetModal from './components/DatasetModal.vue'
 
-const showDatasetModal = ref(true)
+const showDatasetModal = ref(false)
+const datasets: Ref<Dataset[] | undefined> = useObservable(from(liveQuery(() => db.datasets.toArray())))
 </script>
