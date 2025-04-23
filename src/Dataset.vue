d6467ab04a86e708c8c7886189a45130217f20f1
Tal <talrasha007@gmail.com>
2025-04-23 18:18:05 +0800

    add question.

--- src/Dataset.vue
+++ src/Dataset.vue
@@ -1,7 +1,22 @@
 <template>
+  <Row>
+    <Col flex="auto"></Col>
+    <Col flex="none">
+      <Button type="primary" @click="show = true"><FileAddOutlined />Add</Button>
+    </Col>
+  </Row>
+  <br />
   <QuestionTable :dataset_id="parseInt($route.params.dataset_id as string)" />
+  <QuestionModal v-model:show="show" :dataset_id="parseInt($route.params.dataset_id as string)" />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { Row, Col, Button } from 'ant-design-vue'
+import { FileAddOutlined } from '@ant-design/icons-vue'
+
+import QuestionModal from './components/QuestionModal.vue'
 import QuestionTable from './components/QuestionTable.vue'
+
+const show = ref(false)
 </script>
