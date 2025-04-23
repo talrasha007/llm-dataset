b14cd5410f01daeb40b00486f20ab120e1a42e8d
Tal <talrasha007@gmail.com>
2025-04-23 18:29:00 +0800

    del

--- src/components/QuestionTable.vue
+++ src/components/QuestionTable.vue
@@ -2,7 +2,10 @@
   <Table :data-source="questions" :columns="columns" :pagination="false">
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'actions'">
-        <Button type="primary" @click="show = true; question = record as Question">Edit</Button>
+        <Space>
+          <Button type="primary" @click="show = true; question = record as Question"><EditOutlined />Edit</Button>
+          <Button type="primary" danger @click="del(record.id)"><DeleteOutlined /></Button>
+        </Space>
       </template>
     </template>
   </Table>
@@ -12,9 +15,10 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-import { Table, Button } from 'ant-design-vue'
+import { Table, Button, Space } from 'ant-design-vue'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons-vue'
 
-import { useQuestions, type Question } from '../db'
+import db, { useQuestions, type Question } from '../db'
 import QuestionModal from './QuestionModal.vue'
 
 const props= defineProps<{
@@ -28,6 +32,11 @@ let questions = useQuestions(props.dataset_id)
 watch(() => props.dataset_id, () => {
   questions = useQuestions(props.dataset_id)
 })
+
+function del(id: number) {
+  if (confirm('Are you sure to delete this question?'))
+    db.questions.delete(id)
+}
 
 const columns = [
   {
