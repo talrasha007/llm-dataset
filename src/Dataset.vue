6d2ea269ec7f679e1ed9fa3ea3a0319263aab132
Tal <talrasha007@gmail.com>
2025-04-24 17:09:37 +0800

    move script to top.

--- src/Dataset.vue
+++ src/Dataset.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Row, Col, Button } from 'ant-design-vue'
+import { FileAddOutlined } from '@ant-design/icons-vue'
+
+import QuestionModal from './components/QuestionModal.vue'
+import QuestionTable from './components/QuestionTable.vue'
+
+const show = ref(false)
+</script>
+
 <template>
   <Row>
     <Col flex="auto"></Col>
@@ -9,14 +20,3 @@
   <QuestionTable :dataset_id="$route.params.dataset_id as string" />
   <QuestionModal v-model:show="show" :dataset_id="$route.params.dataset_id as string" />
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import { Row, Col, Button } from 'ant-design-vue'
-import { FileAddOutlined } from '@ant-design/icons-vue'
-
-import QuestionModal from './components/QuestionModal.vue'
-import QuestionTable from './components/QuestionTable.vue'
-
-const show = ref(false)
-</script>
